4f4dc129e552d2f95d36ba95f44268e0cfc4c16e
BASIS <71930194+SynitCool@users.noreply.github.com>
2024-02-06 20:29:07 +0700

    Update servoDist.ino

--- Arduino/servoDist.ino
+++ Arduino/servoDist.ino
@@ -3,15 +3,19 @@
 Servo servo;
 
 const int echoPin = A0;
-
+const int lightPin = 12;
 const int triggerPin = 13;
+const int buzzPin = 10;
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
 
   pinMode(echoPin, INPUT);
 
   pinMode(triggerPin, OUTPUT);
+  pinMode(lightPin, OUTPUT);
+  pinMode(buzzPin, OUTPUT);
 
   servo.attach(11);
 }
@@ -22,18 +26,52 @@ void loop() {
 
   int dist = distanceInp();
 
-  if (dist < 10) greetServo();
+  if (dist < 200) {
+    turnOnLight();
+    greetServo();
+    buzz();
+  } else {
+    backServo();
+    turnOffLight();
+  }
 
+  Serial.print("Distance: "); 
+  Serial.print(dist);  
+  Serial.println(" cm");  
+  delay(1000);
+}
+
+void buzz() {
+  for (int i = 0; i <= 10; i++) {
+    digitalWrite(buzzPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(buzzPin, LOW);
+  }
+}
+
+void turnOnLight() {
+  digitalWrite(lightPin, HIGH);
+  delay(1000);
+}
+void turnOffLight(){
+  digitalWrite(lightPin, LOW);
+  delay(1000);
+}
+
+void backServo() {
+  servo.write(0);
   delay(1000);
 }
 
 void greetServo() {
+  // for (int i = 0; i <= 180; i++) {
+  //   servo.write(i); 
+  //   delay(10);
+  // }
   servo.write(0);
-  delay(100);
-  
-  servo.write(180);              
-  delay(150);
-  servo.write(0);
+  delay(1000);
+  servo.write(180);
+  delay(1000);
 }
 
 int distanceInp() {
